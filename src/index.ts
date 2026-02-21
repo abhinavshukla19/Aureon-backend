b1c25888719c3a560a838872f4ebd5099e87a0d8
abhinavshukla19 <ayushabhinav19@gmail.com>
2026-02-21 16:42:37 +0530

    whole api reviewed and major channges done

--- src/index.ts
+++ src/index.ts
@@ -20,17 +20,16 @@ const PORT= process.env.PORT || 3001;
 app.use(express.json())
 app.use(cors())
 
-
-app.use(health)
-app.use(auth);
-app.use(otp);
-app.use(profile)
-app.use(setting)
-app.use(movie)
-app.use(mylist)
-app.use(cast)
-app.use(devices)
-app.use(update)
+app.use("/api/auth", auth);
+app.use("/api/otp", otp);
+app.use("/api/profile", profile);
+app.use("/api/setting", setting);
+app.use("/api/movie", movie);
+app.use("/api/mylist", mylist);
+app.use("/api/cast", cast);
+app.use("/api/devices", devices);
+app.use("/api/update", update);
+app.use("/api", health);
 
 
 const start = async () => {
