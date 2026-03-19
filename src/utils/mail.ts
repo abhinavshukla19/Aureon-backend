f22bb10e83df399988775adb69979d80f51b4108
abhinavshukla19 <ayushabhinav19@gmail.com>
2026-03-19 19:55:53 +0530

    fixed issue

--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -18,8 +18,8 @@ const createTransporter = (): Transporter => {
 
   return nodemailer.createTransport({
     host: "smtp.gmail.com",  
-    port: 587,                
-    secure: false,        
+    port: 465,                
+    secure: true,        
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
